2836e074272fdf3c071ba9949fb83a475f99aa60
Andrea McBride <31637885+amcbride001@users.noreply.github.com>
2018-10-23 19:31:28 -0400

    completed all except .html

--- src/main/java/org/wecancodeit/reviews/Review.java
+++ src/main/java/org/wecancodeit/reviews/Review.java
@@ -3,15 +3,20 @@ package org.wecancodeit.reviews;
 public class Review {
 
 	private long id;
-	private String location;
+	private String title;
 	private String imageUrl;
+	private String category;
+	private String location;
 	private String content;
 	private String name;
 	
 public Review(long id, String location, String name) {
 	this.id = id;
-	this.location = location;
+	this.title = title;
 	this.imageUrl = imageUrl;
+	this.category = category;
+	this.location = location;
+	this.content = content;
 	this.name = name;
 	
 	
@@ -22,9 +27,19 @@ public Long getId() {
 	return id;
 }
 
-public Object getDate() {
+public String getTitle() {
+	return title;
+}
+public String getImage() {
+	return imageUrl;
+}
 
-	return null;
+public String getCategory() {
+	return category;
+}
+
+public String getLocation() {
+	return location;
 }
 public String getContent() {
 	return content;
@@ -32,7 +47,10 @@ public String getContent() {
 public String getName() {
 	return name;
 }
-public String getLocation() {
-	return location;
+public String getDate() {
+
+	return null;
 }
+
 }
+
